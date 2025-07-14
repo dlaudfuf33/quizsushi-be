c890d72ab3077e3a5ff0c4503548d2d57b9fb8fc
dlaudfuf33 <dlaudfuf33@gmail.com>
2025-07-15 02:00:04 +0900

    feat: 챌린지 서비스 및 Redis 통합

--- src/main/java/com/cmdlee/quizsushi/global/config/infra/redis/RedisConfig.java
+++ src/main/java/com/cmdlee/quizsushi/global/config/infra/redis/RedisConfig.java
@@ -1,12 +1,16 @@
 package com.cmdlee.quizsushi.global.config.infra.redis;
 
 import com.cmdlee.quizsushi.member.domain.model.RefreshTokenData;
+import com.cmdlee.quizsushi.quiz.challenge.model.LeaderboardEntry;
+import com.cmdlee.quizsushi.quiz.challenge.model.ChallengeSession;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -32,4 +36,36 @@ public class RedisConfig {
         return template;
     }
 
+    @Bean
+    public RedisTemplate<String, ChallengeSession> challengeSessionRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, ChallengeSession> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        Jackson2JsonRedisSerializer<ChallengeSession> serializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper, ChallengeSession.class);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(serializer);
+        template.afterPropertiesSet();
+
+        return template;
+    }
+
+    @Bean
+    public RedisTemplate<String, LeaderboardEntry> leaderboardRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, LeaderboardEntry> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer(mapper));
+
+        return template;
+    }
 }
