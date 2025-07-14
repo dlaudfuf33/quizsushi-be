575111e96f186c958a08b130c064c26ef4c6e4fc
dlaudfuf33 <dlaudfuf33@gmail.com>
2025-07-15 02:00:19 +0900

    refactor: WebClientConfig를 security 패키지로 이동

--- src/main/java/com/cmdlee/quizsushi/global/config/security/WebClientConfig.java
+++ src/main/java/com/cmdlee/quizsushi/global/config/security/WebClientConfig.java
@@ -1,4 +1,4 @@
-package com.cmdlee.quizsushi.global.config;
+package com.cmdlee.quizsushi.global.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
