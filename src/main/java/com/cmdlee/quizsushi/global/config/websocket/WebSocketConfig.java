a16189e1953fe1e4e7b407acf1bfb4be77a72a83
dlaudfuf33 <dlaudfuf33@gmail.com>
2025-07-16 21:11:59 +0900

    fix: 웹소켓 cors 설정 수정

--- src/main/java/com/cmdlee/quizsushi/global/config/websocket/WebSocketConfig.java
+++ src/main/java/com/cmdlee/quizsushi/global/config/websocket/WebSocketConfig.java
@@ -31,7 +31,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/challenge")
-                .setAllowedOriginPatterns("https://quizsushi.cmdlee.com")
+                .setAllowedOrigins("https://quizsushi.cmdlee.dev", "https://quizsushi.cmdlee.com")
                 .withSockJS();
     }
 }
